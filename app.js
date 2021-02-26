273e626b49906c83e761573c6dc539371117da0a
irtep <rasanen.petri@gmail.com>
2021-02-26 10:12:48 +0200

    regulating

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const config = require('./utils/config');
 const express = require('express');
 const app = express();
 //require('express-async-errors');
-const cors = require('cors');
+//const cors = require('cors');
 const usersRouter = require('./controllers/users');
 const blogsRouter = require('./controllers/blogs');
 const loginRouter = require('./controllers/login');
@@ -20,7 +20,7 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
     logger.error('error connection to MongoDB:', error.message);
   });
 
-app.use(cors());
+//app.use(cors());
 app.use(express.static('build'));
 app.use(express.json());
 app.use(middleware.requestLogger);
